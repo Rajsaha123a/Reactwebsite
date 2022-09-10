eb82d4469fa3bfefac26edd1ec938d255069d3c6
Prithiraj Saha <prithirajsaha503@gmail.com>
2022-09-10 23:12:05 +0530

    Updates

---
+++ precache-manifest.829c87af1cc16140f43bc5b22cf71190.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "85a1adfe5514832d9c3f",
+    "url": "/Reactwebsite/static/css/main.02cc8dd5.chunk.css"
+  },
+  {
+    "revision": "85a1adfe5514832d9c3f",
+    "url": "/Reactwebsite/static/js/main.85a1adfe.chunk.js"
+  },
+  {
+    "revision": "d27391197e9720fc1808",
+    "url": "/Reactwebsite/static/css/1.09bca37a.chunk.css"
+  },
+  {
+    "revision": "d27391197e9720fc1808",
+    "url": "/Reactwebsite/static/js/1.d2739119.chunk.js"
+  },
+  {
+    "revision": "5a50f814d3df92d2972e",
+    "url": "/Reactwebsite/static/js/runtime~main.5a50f814.js"
+  },
+  {
+    "revision": "0eef66fc27bc5b1806ef1fc383f81b22",
+    "url": "/Reactwebsite/static/media/me.0eef66fc.jpg"
+  },
+  {
+    "revision": "9987a31242b8e44ff32d32454c33834b",
+    "url": "/Reactwebsite/static/media/image2.9987a312.jpg"
+  },
+  {
+    "revision": "27f12b4507e491dc46d5253f32f4e8f5",
+    "url": "/Reactwebsite/index.html"
+  }
+];
